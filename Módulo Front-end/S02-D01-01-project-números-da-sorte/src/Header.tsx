6816202ad9d7bf2130d83ee8d69759c37f129648
IMatheusmaia <i.matheuslmaia@gmail.com>
2023-08-08 16:10:36 -0300

    add project numeros da sorte

---
+++ Módulo Front-end/S02-D01-01-project-números-da-sorte/src/Header.tsx
@@ -0,0 +1,13 @@
+import Greeting from "./Greeting";
+import LuckyNumbers from "./LuckyNumbers";
+
+function Header() {
+  return (
+    <>
+      <Greeting/>
+      <LuckyNumbers/>
+    </>
+  );
+}
+
+export default Header;
