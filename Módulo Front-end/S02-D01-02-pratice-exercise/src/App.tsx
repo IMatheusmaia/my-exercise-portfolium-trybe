11a3fae3766ff03aec01a77f284ac6666b88031b
IMatheusmaia <i.matheuslmaia@gmail.com>
2023-08-09 10:05:08 -0300

    completed exerciese

--- Módulo Front-end/S02-D01-02-pratice-exercise/src/App.tsx
+++ Módulo Front-end/S02-D01-02-pratice-exercise/src/App.tsx
@@ -1,6 +1,21 @@
-function App() {
+import HelloWorld from "./HelloWorld";
+import calculeAge from "./utils/age";
 
-  return '';
+function App() {
+  const name = 'Ícaro Maia';
+  const birthday = '02/12/1995';
+  return (
+    <>
+      <HelloWorld/>
+      <h2>Calcule sua idade:</h2>
+      <ul>
+        <li>{ name }</li>
+        <li>{ birthday }</li>
+        <li>Idade: {calculeAge(birthday)}</li>
+      </ul>
+      
+    </>
+  );
 }
 
 export default App
---
+++ Módulo Front-end/S02-D01-02-pratice-exercise/src/HelloWorld.tsx
@@ -0,0 +1,16 @@
+
+import Title from "./Title";
+import ModuleDetails from "./ModuleDetails";
+
+function HelloWorld() {
+  const name = 'John Connor';
+  const birth = '11/02/1989';
+    return (
+        <>
+          <Title/>
+          <ModuleDetails/>
+        </>
+    )
+}
+
+export default HelloWorld;
